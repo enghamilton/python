5ee051b1550c70b20691c0e99fb5b72698c88fb0
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-09-13 03:19:24 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,49 @@
+# from this site http://kazuar.github.io/scraping-tutorial/
+
+import requests
+from lxml import html
+from flask import Flask
+
+USERNAME = "enghamilton"
+PASSWORD = "p4dcGj97"
+
+LOGIN_URL = "https://github.com/login"
+URL = "https://github.com/"
+
+def main():
+    session_requests = requests.session()
+
+    # Get login csrf token
+    result = session_requests.get(LOGIN_URL)
+    tree = html.fromstring(result.text)
+
+    # Create payload
+    payload = {
+        "username": USERNAME, 
+        "password": PASSWORD
+    }
+
+    # Perform login
+    result = session_requests.post(LOGIN_URL, data = payload, headers = dict(referer = LOGIN_URL))
+
+    # Scrape url
+    result = session_requests.get(URL, headers = dict(referer = URL))
+    tree = html.fromstring(result.content)
+    bucket_names = tree.xpath("//span[@class='ver text here']/a/text()")
+
+    return (result.content)
+
+f = open("myscraped.txt", "w")
+f.write(main())
+f.close()		
+
+if __name__ == '__main__':
+    main()
+
+app = Flask(__name__)
+
+@app.route('/')
+def source():
+  if __name__ == '__main__':
+  html = main()
+  return html
